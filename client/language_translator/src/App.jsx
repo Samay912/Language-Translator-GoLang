c1c36f6dbddc710c0948a6436907b5fd43feed56
RISHITH125 <rishithp125@gmail.com>
2024-04-11 23:20:28 +0530

    complete UI overhaul

--- client/language_translator/src/App.jsx
+++ client/language_translator/src/App.jsx
@@ -4,37 +4,48 @@ import List from './List';
 function App() {
   const [data, setData] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [isReceiving, setIsReceiving] = useState(false);
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       const text = event.target.value.trim();
       if (text !== '') {
+        setIsReceiving(true);
         const socket = new WebSocket('ws://localhost:8050/websoc');
         socket.onopen = () => {
           if (socket.readyState === 1) {
             socket.send(text);
-            setInputValue('');
+            setInputValue('receiving...');
             setData([])
           }
         };
         socket.onmessage = (event) => {
           setData((prevData) => [...prevData, event.data]);
+          setIsReceiving(false);
+          setInputValue('Enter in any language...');
         };
       }
     }
   };
 
   return (
-    <div id="main">
+    <div id="main" className="w-screen h-screen flex flex-col items-center justify-center overflow-x-hidden">
+      <h1 className="text-center text-4xl font-bold">LANGUAGE TRANSLATOR</h1>
+      <h2 className="font-semibold text-2xl">Enter a text to translate:</h2>
       <input
         type="text"
         id="text-input"
-        placeholder="Enter text to translate"
+        placeholder="Enter in any language..."
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         onKeyPress={handleKeyPress}
+        className="border-black border-2 w-1/3 h-10 p-2 mt-2 text-lg"
+        disabled={isReceiving}
       />
-      <List data={data} />
+      <div className="max-h-[50vh] overflow-auto mt-4">
+        <List data={data} className=""/>
+      </div>
+      
     </div>
   );
 }
